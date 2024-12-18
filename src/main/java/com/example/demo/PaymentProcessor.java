76b150867e6051420b98cc2560f7ef038f2ef20b
Takudai Kawasaki tkawasaki <takudai0717@gmail.com>
2024-12-18 22:41:11 +0900

    refactor PaymentProcessor and PaymentStrategy for improved clarity and functionality

--- src/main/java/com/example/demo/PaymentProcessor.java
+++ src/main/java/com/example/demo/PaymentProcessor.java
@@ -1,18 +1,14 @@
 package com.example.demo;
 
-public class PaymentContext {
+public class PaymentProcessor {
 
   private PaymentStrategy paymentStrategy;
-
-  public PaymentContext(PaymentStrategy paymentStrategy) {
-    this.paymentStrategy = paymentStrategy;
-  }
 
   public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
     this.paymentStrategy = paymentStrategy;
   }
 
-  public void performPay(double amount) {
-    paymentStrategy.pay(amount);
+  public void processPayment(double amount) {
+    paymentStrategy.processPayment(amount);
   }
 }
--- src/main/java/com/example/demo/PaymentStrategy.java
+++ src/main/java/com/example/demo/PaymentStrategy.java
@@ -2,5 +2,5 @@ package com.example.demo;
 
 public interface PaymentStrategy {
 
-  void pay();
+  void processPayment(double amount);
 }
